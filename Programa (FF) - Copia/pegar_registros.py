c309cd87042de565a4489a43781c6a9a0a53ff48
Tayotoshi <48145131+Tayotoshi@users.noreply.github.com>
2019-03-05 03:22:08 -0300

    Update pegar_registros.py

--- Programa (FF) - Copia/pegar_registros.py
+++ Programa (FF) - Copia/pegar_registros.py
@@ -1,9 +1,17 @@
-#Cria um arquivo.txt e armazena a lista de registros (lista_registros)
-def cria_e_salva_arquivo (lista_registros):
-    x=0
-    arq = open('registros.json', 'a',encoding='utf-8')
-    texto = lista_registros
-    while x < len(lista_registros):
-        arq.write(texto[x])
-        x += 1
-    arq.close()
+import jsonlines
+
+#Armazena os registros em um arquivo JSONL
+def gerando_arquivo_json(lista_registros):
+    cep_inicial = 1
+    cidade_inicial = 0
+    for registros in range(0, len(lista_registros),4):
+        registros={}
+        registros["Registro"]={
+            'Localidade':lista_registros[cidade_inicial],
+            'Faixa_Cep':lista_registros[cep_inicial]
+            }
+        s=registros
+        with jsonlines.open("registros.jsonl","a") as a:
+            a.write(s)
+            cidade_inicial += 4
+            cep_inicial += 4
